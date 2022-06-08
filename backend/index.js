9afca8ce201f7e92359f1f4694d2f3bfa2817651
viviennoel <vivien.noel@live.fr>
2022-06-08 17:52:59 +0200

    fix: (CICD) test backend

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('Hello from Lambda test!'),
     };
     return response;
 };
